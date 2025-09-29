0bbf7a00596e0e58de392cece73bb24fa2fb23b3
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-09-29 22:28:00 +0530

    modify print statement

--- try.py
+++ try.py
@@ -3,5 +3,7 @@ user_input = int(input("Enter a nubmer: "))
 
 if user_input % 2 == 0:
     print("It's a even number.")
+    print("It's divisible by 2")
 else:
     print("It's a odd number")
+    print("It's not divisible by 2")
