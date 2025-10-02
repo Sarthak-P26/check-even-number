9ec5a07d9015d8b24cde3959ece89ae21de9399e
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-10-02 18:07:26 +0530

    create a checking fucntion

--- try.py
+++ try.py
@@ -1,9 +1,11 @@
 print("Welcom to my program")
 user_input = int(input("Enter a nubmer: "))
+def even_or_not(user_input):
+    if user_input % 2 == 0:
+        print("It's a even number.")
+        print("It's divisible by 2")
+    else:
+        print("It's a odd number")
+        print("It's not divisible by 2")
 
-if user_input % 2 == 0:
-    print("It's a even number.")
-    print("It's divisible by 2")
-else:
-    print("It's a odd number")
-    print("It's not divisible by 2")
+even_or_not(user_input)
